987ea745c3065bdba12a879b0f2ace495fe2faaa
Brian Manning Windows <manningbrian98@gmail.com>
2021-02-17 11:38:34 +0000

    added package for installation on ec2

---
+++ package/README.md
@@ -0,0 +1,1 @@
+Data Harvester for Dublin Bikes Project.
---
+++ package/build/lib/dublinbikes_harvester/my_secrets.py
@@ -0,0 +1,8 @@
+import os
+
+DB_PASS=os.environ("DB_PASS")
+DB_URI=os.environ("DB_URI")
+DB_PORT=os.environ("DB_PORT")
+DB_DB=os.environ("DB_DB") 
+DB_USER=os.environ("DB_USER")
+APIKEY=os.environ("APIKEY")
