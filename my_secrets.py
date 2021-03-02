e9f13d203972696fc369a275124a5d41ebef368d
Brian Manning <manningbrian98@gmail.com>
2021-03-02 18:14:12 +0000

    removed unused files

--- my_secrets.py
+++
@@ -1,7 +0,0 @@
-DB_USER='admin'
-DB_PASS='DatabaseSQL1'
-DB_URI='dublinbikes.cho9zjfno44f.us-east-1.rds.amazonaws.com'
-APIKEY='06af51839f293cccf02a9ba5f4204bb094506a95A'
-WAPIKEY='74571a8a54ecc764ebed7d0dc725ebe2'
-NAME="Dublin"
-STATIONS="https://api.jcdecaux.com/vls/v1/stations?"
