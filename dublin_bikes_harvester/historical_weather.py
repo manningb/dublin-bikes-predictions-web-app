ac23d2399a623a06c8fe357c59c1a91e643f704f
Brian Manning <manningbrian98@gmail.com>
2021-03-25 10:30:14 +0000

    Added historical weather scraper

--- dublin_bikes_harvester/historical_weather.py
+++ dublin_bikes_harvester/historical_weather.py
@@ -30,16 +30,15 @@ connection = engine.connect()
 # create current weather forecast table
 sql_create_table_historical_daily = """
 CREATE TABLE IF NOT EXISTS `dublin_bikes`.`weather_historical_daily` (
-station_number INTEGER,
 time_queried DATETIME,
 last_update DATETIME,
-temp INTEGER,
-feels_like INTEGER,
-pressure INTEGER,
-humidity INTEGER,
-visibility INTEGER,
-wind_speed INTEGER,
-wind_deg INTEGER,
+temp FLOAT,
+feels_like FLOAT,
+pressure FLOAT,
+humidity FLOAT,
+visibility FLOAT,
+wind_speed FLOAT,
+wind_deg FLOAT,
 weather_main VARCHAR(256),
 weather_description VARCHAR(256)
 )
@@ -59,41 +58,36 @@ def create_tables():
         print(e)
 
 
-def get_data(data, station_number):
+def get_data(data):
     """
     this function takes in the data and station number as input and returns a tuple with that data
     """
     now = datetime.datetime.utcnow()
-    return (station_number, now, datetime.datetime.fromtimestamp(data.get("dt")), data.get("main").get("temp"), data.get("main").get("feels_like"), data.get("main").get("pressure"), data.get("main").get("humidity"), data.get("visibility"), data.get("wind").get("speed"), data.get("wind").get("deg"), data.get("weather")[0].get("main"), data.get("weather")[0].get("description"))
+    return (now, datetime.datetime.fromtimestamp(data.get("dt")), data.get("main").get("temp"), data.get("main").get("feels_like"), data.get("main").get("pressure"), data.get("main").get("humidity"), data.get("visibility"), data.get("wind").get("speed"), data.get("wind").get("deg"), data.get("weather")[0].get("main"), data.get("weather")[0].get("description"))
 
 
-def loop_through_stations(text):
+def loop_through_stations():
     """
     loops through each of the stations in the dublin bikes static data
     stores relevant values in response to variables
     requests data from Open Weather Map API and import this data to the database
     takes an input text file of Dublin Bikes stations
     """
-    stations = json.loads(text)
-    # loop through each dublin bikes station
-    for station in stations:
-        # print out name for debugging
-        print(station.get("name"))
+    station_lng = -6.266802
+    station_lat = 53.344007
+    loop_through_2020_by_week(station_lng, station_lat, start_week=0, end_week=52)
 
-        # get dublin bikes variables
-        station_number = station.get("number")
-        station_lng = station.get("position").get("lng")
-        station_lat = station.get("position").get("lat")
-        loop_through_2020_by_week(station_number, station_lng, station_lat)
-
-def loop_through_2020_by_week(station_number, station_lng, station_lat):
+def loop_through_2020_by_week(station_lng, station_lat, start_week, end_week):
     """
     Loops through all dates in 2020 by week as this is max api request
     """
     one_week = 604800 # one week in UTC
     # initialise the very start start of the script
     # this time represents 01/01/2020 00:00:00
-    start_date = 1583020800
+
+    start_date = 1585138591 + (start_week * one_week)
+    print(start_date)
+
     end_date = start_date + one_week
     for i in range(52):
         #payload = {'start_dat': start_date, 'key2': 'value2'}
@@ -107,8 +101,8 @@ def loop_through_2020_by_week(station_number, station_lng, station_lat):
         days = data.get("list")
         for day in days:
             print(day)
-            vals = get_data(day, station_number)
-            engine.execute("INSERT INTO `dublin_bikes`.`weather_historical_daily` values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",vals)
+            vals = get_data(day)
+            engine.execute("INSERT INTO `dublin_bikes`.`weather_historical_daily` values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",vals)
             print(vals)
 
 def get_dublin_bikes_stations():
@@ -124,18 +118,16 @@ def main():
     # run create tables once
     create_tables()
     #print(os.path)
-
-    while True:
-        try:
-            stations = get_dublin_bikes_stations().text
-            loop_through_stations(stations)
-            time.sleep(5 * 60)
-        except:
-            print(traceback.format_exc())
-            print("Error found an error when querying")
-            time.sleep(5 * 60)
-            # if engine is None:
-            # pass
+    try:
+        #stations = get_dublin_bikes_stations().text
+        loop_through_stations()
+        time.sleep(5 * 60)
+    except:
+        print(traceback.format_exc())
+        print("Error found an error when querying")
+        time.sleep(5 * 60)
+        # if engine is None:
+        # pass
 
 if __name__ == "__main__":
     main()
